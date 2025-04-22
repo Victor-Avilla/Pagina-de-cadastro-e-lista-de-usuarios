f59aa95ee63df927d5d9bb604be3f6295c5a0c81
Victor-Avilla <victorfonsecaavila@gmail.com>
2025-04-22 19:54:57 -0300

    CSS iniciado

--- src/styles.js
+++ src/styles.js
@@ -7,6 +7,22 @@ export const Title = styled.h2`
 export const Form = styled.form``
 export const ContainerInputs = styled.div``
 export const Input = styled.input``
-export const Button = styled.button``
+export const Button = styled.button`
+    font-size: 16px;
+    color:white;
+    border: none;
+    width:fit-content;
+    padding:16px 32px;
+    border-radius:25px;
+    cursor: pointer;
+    background:linear-gradient(50deg,rgb(58, 158, 180) 0%,
+     rgba(253, 29, 29, 1) 30%, rgba(252, 176, 69, 1) 100%);
+     &:hover{
+         opacity:0.8;
+     }
+     &:active{
+        opacity:0.6;
+     }
+     `
 export const TopBackground = styled.div``
 export const InputLabel = styled.label``
